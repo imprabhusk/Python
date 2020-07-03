cb6315687b5de1ec6e8791891f441f7a2b1122d2
prabhusk <prabhu.sk.169@gmail.com>
2020-07-03 20:49:38 +0530

    Letter Counter App program added.

--- Basic Data Types/LetterCountingApp.py
+++ Basic Data Types/LetterCountingApp.py
@@ -7,3 +7,21 @@ an occurence of h. Your program will then display a message to the user
 stating the occurrences of the given letter.
 
 """
+
+print("Welcome to the Letter Counter App")
+
+name = input("\nWhat is your name : ").title().strip()
+print("Hello", name.capitalize() + "!")
+
+print(
+    "\nI will count the number of times that a specific letter occurs in a \
+message"
+)
+
+message = input("\nPlease Enter a Message : ")
+letter = input("\nWhich letter would you like to count the occurence of : ")
+
+message = message.lower()
+letter = letter.lower()
+letter_count = message.count(letter)
+print(f"\n{name}, your message has {letter_count} {letter}'s in it.")
