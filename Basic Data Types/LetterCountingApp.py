05e550ae35988163f533a501c40e1ca851bc329a
prabhusk <prabhu.sk.169@gmail.com>
2020-07-03 20:07:56 +0530

    Letter Counting App Description Added.

---
+++ Basic Data Types/LetterCountingApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will get a message and a
+specific letter from a user and then count the number of occurrences of
+that letter in the given message. You program should count “H” and “h” as
+an occurence of h. Your program will then display a message to the user
+stating the occurrences of the given letter.
+
+"""
