5b9dd226de83e8b2e29178f1979ffd218091b1b8
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 11:27:36 +0530

    Temperature Conversion App Description Added.

---
+++ Basic Data Types/TemperatureConversionApp.py
@@ -0,0 +1,24 @@
+""" Description:
+
+You are responsible for writing a program that will convert a given
+temperature in degrees Fahrenheit to degrees Celsius and degrees Kelvin. Your
+program will round all conversions to a precision of four decimal places.
+Lastly, your program will display the results in a convenient table style
+format.
+
+Step by Step Guide:
+
+● Print a welcome message.
+● Get user input for their given temperature in degrees Fahrenheit.
+    ○ Allow for a decimal temperature.
+● Convert the temperature into both Celsius and Kelvin.
+    ○ If you are unsure of the conversion ratios, google is your friend.
+    ○ Round all values to 4 decimal places.
+● Display all three temperatures such that the temperature values are
+  aligned when printing.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output as below.
+
+"""
