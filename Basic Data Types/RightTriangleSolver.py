760c9a4e8bfe9bc6194f8ca106cd8c6d27084f57
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 12:35:29 +0530

    Right Triangle Solver steps added.

--- Basic Data Types/RightTriangleSolver.py
+++ Basic Data Types/RightTriangleSolver.py
@@ -4,4 +4,26 @@ You are responsible for writing a program that will calculate the hypotenuse
 and area of a right triangle given its two bases. Your program will round all
 calculations to a precision of three decimal places and provide a summary of
 the mathematical results.
+
+Step by Step Guide:
+
+● Print a welcome message.
+● Get user input for the first side of the right triangle.
+● Get user input for the second side of the right triangle.
+● Calculate the hypotenuse of the right triangle using the Pythagorean theorem.
+    ○ We can't actually take a square root with basic Python. In order to take
+      a square root, we will need to import a library of extra code.
+    ○ Type import math as the first line of code in your program.
+    ○ This allows us to access higher level mathematical functions such as the
+      square root function sqrt().
+    ○ Google how to take a square root using the math library.
+● Calculate the area of the right triangle.
+● Round each value to 3 decimal places.
+● Print a message to the user informing them of both the hypotenuse and area of
+  the given triangle.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output as below.
+
 """
