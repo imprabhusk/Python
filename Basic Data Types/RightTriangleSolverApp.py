ffa6cb42e8584c77622210f09f3a8a7122f0e5ac
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 12:59:31 +0530

    Right Triangle Solver App program added.

--- Basic Data Types/RightTriangleSolverApp.py
+++ Basic Data Types/RightTriangleSolverApp.py
@@ -8,8 +8,8 @@ the mathematical results.
 Step by Step Guide:
 
 ● Print a welcome message.
-● Get user input for the first side of the right triangle.
-● Get user input for the second side of the right triangle.
+● Get user input for the side_a of the right triangle.
+● Get user input for the side_b of the right triangle.
 ● Calculate the hypotenuse of the right triangle using the Pythagorean theorem.
     ○ We can't actually take a square root with basic Python. In order to take
       a square root, we will need to import a library of extra code.
@@ -41,5 +41,8 @@ hypotenuse = math.sqrt(side_a ** 2 + side_b ** 2)
 area = 0.5 * side_a * side_b
 
 # format the output
-print(f"\nHypotenuse for the sides {side_a} and {side_b} is {round(hypotenuse, 3)}")
+print(
+    f"\nHypotenuse for the sides {side_a} and {side_b} is\
+  {round(hypotenuse, 3)}"
+)
 print(f"\nArea of the Triangle for the {side_a} and {side_b} is {area}")
