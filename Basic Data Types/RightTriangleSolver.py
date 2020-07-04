2ff7f4ce4397afefed8d7c7ef8bf07366c4b804e
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 12:30:56 +0530

    Right Triangle solver Description Added.

---
+++ Basic Data Types/RightTriangleSolver.py
@@ -0,0 +1,7 @@
+""" Description:
+
+You are responsible for writing a program that will calculate the hypotenuse
+and area of a right triangle given its two bases. Your program will round all
+calculations to a precision of three decimal places and provide a summary of
+the mathematical results.
+"""
