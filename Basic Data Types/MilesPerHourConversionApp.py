b3dcc765483cd525aca38800a436180a15ea9ce3
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 10:57:00 +0530

    Miles Per Hour Conversion App Steps Added.

--- Basic Data Types/MilesPerHourConversionApp.py
+++ Basic Data Types/MilesPerHourConversionApp.py
@@ -5,4 +5,21 @@ speed in miles per hour to a more metric friendly unit of meters per
 second. All calculations should be rounded to a set decimal precision of 2
 decimal places.
 
+Step by Step Guide:
+
+● Print a welcome message.
+● Get user input for their speed in miles per hour.
+    ○ Allow for a decimal speed.
+● Convert the speed in miles per hour to meters per second.
+    ○ Use a conversion ratio of 1 mph = 0.4474 mps.
+    ○ Use the round() function to round this speed to 2 decimal places.
+    ○ Check the python documentation for information on how to use the round()
+      function.
+● Print a message to the user that informs them of their speed in meters per
+  second.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output.
+
 """
