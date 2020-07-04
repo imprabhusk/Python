ab2f45480280c0c019e50520754bc4a2b7306a81
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 13:09:50 +0530

    Multiplication and Exponentiation Description added.

---
+++ Basic Data Types/MultiplicationAndExponentiationApp.py
@@ -0,0 +1,8 @@
+""" Description
+
+You are responsible for writing a program that displays a multiplication table
+and exponentiation table for any given number. Each table should show
+mathematical results for operations performed with the given number and
+integers from 1 to 9. The program will then print a series of messages to the
+user describing how cool mathematics truly is.
+"""
