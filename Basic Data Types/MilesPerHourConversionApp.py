bc89abd224f8efad9614ab84b6f5467222ed51f8
prabhusk <prabhu.sk.169@gmail.com>
2020-07-04 10:42:56 +0530

    Miles Per Hour Conversion App Description Added.

---
+++ Basic Data Types/MilesPerHourConversionApp.py
@@ -0,0 +1,8 @@
+""" Description:
+
+You are responsible for writing a program that will convert any given
+speed in miles per hour to a more metric friendly unit of meters per
+second. All calculations should be rounded to a set decimal precision of 2
+decimal places.
+
+"""
