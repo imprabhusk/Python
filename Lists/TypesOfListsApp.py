a12597845070f0d9557aec9e7682b94b4e7f70d3
prabhusk <prabhu.sk.169@gmail.com>
2020-07-05 11:02:10 +0530

    Types of Lists Description Added.

---
+++ Lists/TypesOfListsApp.py
@@ -0,0 +1,11 @@
+""" Description:
+
+You are responsible for writing a program that will highlight the similarities
+and differences between four different types of lists: a list of strings, a
+list of integers, a list of floats, and a list of lists. For each list, your
+program will describe the data type of the list, the elements of the list, and
+the data type of the first element in the list. Your program will then
+highlight the similarities and differences between sorting a list numerically
+and alphabetically.
+
+"""
