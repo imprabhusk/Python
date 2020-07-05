6de4bd9d57f6d44a669d577d198663e610e648b8
prabhusk <prabhu.sk.169@gmail.com>
2020-07-05 11:05:13 +0530

    Types of Lists Steps Added.

--- Lists/TypesOfListsApp.py
+++ Lists/TypesOfListsApp.py
@@ -8,4 +8,26 @@ the data type of the first element in the list. Your program will then
 highlight the similarities and differences between sorting a list numerically
 and alphabetically.
 
+Step by Step Guide:
+
+● Define a list using a variable num_strings and "hard code" the following four
+  numerical strings: "15", "100", "55", "42".
+● Define a list using a variable num_ints and hard code the following four
+  numerical integers: 15, 100, 55, 42.
+● Define a list using a variable num_floats and hard code any four floats you want.
+● Define a list using a variable num_lists. This is going to be a lists of lists
+  or a nested list!
+  Use the following syntax: num_lists = [[1,2,3], [4,5,6], [7,8,9]]
+● Print a summary of each variable (or list). The summary should contain:
+    ○ A statement about the variable's type.
+    ○ A statement about the elements of the variable.
+    ○ A statement about the first element and its type.
+    ○ Use formatting below.
+● Permanently sort num_strings and num_ints.
+● Print each list.
+● Print a statement about what you discover when sorting these two lists.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output as below.
 """
