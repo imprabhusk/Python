c687e188092616ae698f9b38f2075696b52f114b
prabhusk <prabhu.sk.169@gmail.com>
2020-07-05 10:37:34 +0530

    Grade Sorter App Steps Added.

--- Lists/GradeSorterApp.py
+++ Lists/GradeSorterApp.py
@@ -5,4 +5,23 @@ user. Your program will then sort these grades from highest to lowest. Then,
 your program will simulate dropping the lowest two grades the user entered.
 Lastly, it will comment on the users highest grade.
 
+Step By Step Guide:
+
+● Print a welcome message.
+● Create a blank list called grades.
+● Get user input to add 4 grades to the list.
+    ○ Be aware of the data type you are using.
+● Print the list as formatted below.
+    ○ You may have to cast the list to a string.
+● Permanently sort the list from highest to lowest.
+● Inform the user that their lowest two grades are being dropped.
+● Remove the lowest two grades from the list.
+● Print a message informing the user that the grades were dropped.
+● Print the remaining grades.
+● Print the users highest grade with a message.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output as below.
+
 """
