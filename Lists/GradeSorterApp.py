10c2587739a3a6976666aee0348cebaf8a478067
prabhusk <prabhu.sk.169@gmail.com>
2020-07-05 10:58:15 +0530

    Grade Sorter App Comments Added.

--- Lists/GradeSorterApp.py
+++ Lists/GradeSorterApp.py
@@ -28,9 +28,13 @@ Step By Step Guide:
 
 print("Welcome to Grade Sorter App")
 
+# Initialize the list
 grades = []
 
+# Gather grades from the user
 grade = int(input("\nWhat is your first grade (0 - 100) : "))
+
+# Append grades to the list
 grades.append(grade)
 grade = int(input("What is your second grade (0 - 100) : "))
 grades.append(grade)
@@ -39,19 +43,21 @@ grades.append(grade)
 grade = int(input("What is your fourth grade (0 - 100) : "))
 grades.append(grade)
 
+# print all the grades entered by the user
 print(f"\nYour grades are {grades}")
 
+# Sort the list from highest to lowest
 grades.sort(reverse=True)
 
 print(f"\nYour Grades from highest to lowest : {grades}")
 
+# Removing the lowest two grades
 print("\nThe Lowest two grades will now be dropped")
-
+removed_grade = grades.pop()
+print(f"\nRemoved Grade is : {removed_grade}")
 removed_grade = grades.pop()
 print(f"\nRemoved Grade is : {removed_grade}")
 
-removed_grade = grades.pop()
-print(f"\nRemoved Grade is : {removed_grade}")
-
+# format the output
 print(f"\nYour Remaining Grades are : {grades}")
 print(f"\nCongratulations, Your Highest Grade is : {grades[0]}")
