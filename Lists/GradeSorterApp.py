4f8d8fc9fda47bc33349a32cc6c1b7966e14e296
prabhusk <prabhu.sk.169@gmail.com>
2020-07-05 10:33:29 +0530

    Grade Sorter App Description Added.

---
+++ Lists/GradeSorterApp.py
@@ -0,0 +1,8 @@
+""" Description:
+
+You are responsible for writing a program that will collect four grades from a
+user. Your program will then sort these grades from highest to lowest. Then,
+your program will simulate dropping the lowest two grades the user entered.
+Lastly, it will comment on the users highest grade.
+
+"""
