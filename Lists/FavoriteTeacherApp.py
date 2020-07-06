64a972e4138c641b9dde8502c57a3f5e7aadbc52
prabhusk <prabhu.sk.169@gmail.com>
2020-07-06 20:49:57 +0530

    FavoriteTeacher App steps added.

--- Lists/FavoriteTeacherApp.py
+++ Lists/FavoriteTeacherApp.py
@@ -8,4 +8,51 @@ teachers, the next two teachers, the last favorite teacher, and the total
 number of favorite teachers in the list. Your program will then add and remove
 teachers from this list, each time displaying a similar summary.
 
+Step By Step Guide:
+
+● Print a welcome message.
+● Create a blank list called fav_teachers.
+● Prompt the user to enter in their four favorite teachers.
+    ○ The user should only enter the last name of the teacher.
+    ○ Make sure to title case all user input for sorting purposes.
+● Add each of these teachers to the list fav_teachers.
+● Print the favorite teachers ranked.
+● Print the favorite teachers alphabetically.
+● Print the favorite teachers in reverse alphabetical order.
+● Print the top two teachers.
+● Print the next two teachers.
+● Print the last favorite teacher.
+● Print the total number of favorite teachers.
+● Follow the formatting below.
+
+● Print a message to the user informing them that first favorite teacher is no longer
+  their favorite teacher.
+● Prompt the user to enter their new favorite teacher.
+    ○ Store the name of this teacher in the first index of the list
+    ○ Shift all other teachers down one index.
+● Print the favorite teachers ranked.
+● Print the favorite teachers alphabetically.
+● Print the favorite teachers in reverse alphabetical order.
+● Print the top two teachers.
+● Print the next two teachers.
+● Print the last favorite teacher.
+● Print the total number of favorite teachers.
+● Follow the formatting below.
+
+● Print a message to the user informing them that they no longer like a teacher.
+● Prompt the user to pick a teacher to remove from the list.
+● Remove this teacher.
+● Print the favorite teachers ranked.
+● Print the favorite teachers alphabetically.
+● Print the favorite teachers in reverse alphabetical order.
+● Print the top two teachers.
+● Print the next two teachers.
+● Print the last favorite teacher.
+● Print the total number of favorite teachers.
+● Follow the formatting below.
+● Use at least 2 comments to describe sections of your code
+● “Chunk” your so that it is readable.
+● Use appropriate and informative variable names.
+● Format your output.
+
 """
