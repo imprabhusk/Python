d50369ed7d1337ef6d4da5ba27c0822ea2a51d75
prabhusk <prabhu.sk.169@gmail.com>
2020-07-06 20:14:20 +0530

    BasketBall Roaster App Steps Added.

--- Lists/BasketBallRoasterApp.py
+++ Lists/BasketBallRoasterApp.py
@@ -6,4 +6,32 @@ injury to a specific player in the roster and prompt the user to update the
 roster. Upon updating the roster, your program will display the final roster
 and wish the newly add player good luck.
 
+
+Step by Step Guide:
+
+● Print a welcome message.
+● Create a blank list called roster.
+● Get user input for the names of the starting roster for a basketball team.
+    ○ The starting roster includes a point guard, shooting guard, small forward, power
+      forward, and center. Use an input statement for each position.
+    ○ Make sure to always display the name capitalized.
+    ○ Add each position to your list roster.
+    ○ Your point guard should be index 0, shooting guard at index 1, ect...
+● Print the starting 5 as formatted below.
+● Remove the small forward from the list and store it in a variable called injured_player.
+● Print a message to the user informing them that this player is injured.
+● Get the length of the current roster.
+● Print a message to the user informing them of the length of the current roster.
+● Get user input for who will take the injured players spot and store this in a variable
+  called added_player.
+● Add this player to the roster at the correct position.
+● Print the updated starting 5 as formatted below.
+● Print a good luck message to the new player.
+● Get the length of the current roster.
+● Print a message to the user informing them of the length of the current roster.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output.
+
 """
