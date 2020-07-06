99d9fa062563d9bfe65faee8a8db0b6b844b5435
prabhusk <prabhu.sk.169@gmail.com>
2020-07-06 20:07:05 +0530

    BasketBall Roaster App Description Added.

---
+++ Lists/BasketBallRoasterApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will build and display a
+basketball roster based off user input. Your program will then simulate an
+injury to a specific player in the roster and prompt the user to update the
+roster. Upon updating the roster, your program will display the final roster
+and wish the newly add player good luck.
+
+"""
