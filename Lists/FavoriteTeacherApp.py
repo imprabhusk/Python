7e075fd9484ac0217ff63dc90438c4fd85ce1119
prabhusk <prabhu.sk.169@gmail.com>
2020-07-06 20:44:47 +0530

    FavoriteTeacher App Description Added.

---
+++ Lists/FavoriteTeacherApp.py
@@ -0,0 +1,11 @@
+""" Description:
+
+You are responsible for writing a program that will create a list of a user’s
+favorite teachers. It will display these teachers ranked (assuming the first
+teacher entered is the favorite, the second teacher entered is the next
+favorite, ect...), alphabetically, in reverse alphabetical order, the top two
+teachers, the next two teachers, the last favorite teacher, and the total
+number of favorite teachers in the list. Your program will then add and remove
+teachers from this list, each time displaying a similar summary.
+
+"""
