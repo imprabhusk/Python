024360cd42592ff55887ef24efc36b47b5f279bc
prabhusk <prabhu.sk.169@gmail.com>
2020-07-06 19:06:22 +0530

    Grocery List App Description Added.

---
+++ Lists/GroceryListApp.py
@@ -0,0 +1,13 @@
+""" Description:
+
+You are responsible for writing a program that will simulate a grocery
+shopping list. Your program will start with two items on the shopping list,
+meat and cheese, and then allow a user to add three new items to the list. To
+simulate shopping, your program will ask the user what item they just
+purchased and then remove the item from the shopping list. Upon having only
+two items in the shopping list, your program will inform the user that the
+store is out of a particular item and prompt the user to replace the item with
+a new item. You will use the datetime library to display the current date and
+time the shopping is taking place in mm/dd hh:mm format.
+
+"""
