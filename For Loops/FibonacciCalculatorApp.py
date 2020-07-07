9840ece27158c401bf28a3fcb45b9ff9ca917419
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 14:13:16 +0530

    Fibonacci calculator app description added.

---
+++ For Loops/FibonacciCalculatorApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will compute the first n terms
+of the Fibonacci Sequence. Your program will then display these terms. Next,
+your program will calculate the ratios of consecutive Fibonacci numbers to
+prove that these ratios approach the irrational mathematical constant of Phi;
+1.618....
+
+"""
