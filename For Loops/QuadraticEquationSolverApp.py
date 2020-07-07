68a15f248865dcee9b22d31041109e140bfb1dd3
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 13:01:06 +0530

    Quadratic Equation Solver app description added.

---
+++ For Loops/QuadraticEquationSolverApp.py
@@ -0,0 +1,10 @@
+""" Description:
+
+You are responsible for writing a program that will display the solutions to
+any number of quadratic equations. Your program will ask the user how many
+quadratic equations they would like to solve, ask for the coefficients of the
+equation in the standard form of ax2 + bx + c = 0 , solve for x, and then
+display the solutions. Your program will allow for both real and complex
+solutions.
+
+"""
