1b800d361f1082ba57a680ea5465da6e13b39953
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 14:31:14 +0530

    Fibonacci calculator app comments added.

--- For Loops/FibonacciCalculatorApp.py
+++ For Loops/FibonacciCalculatorApp.py
@@ -40,23 +40,27 @@ compute.
 
 print("Welcome to Fibonacci Calculator App")
 
+# Get user input
 number = int(input('\nHow many digits of the fibonacci sequence would you like to compute : '))
 
+# compute the values of the fib
 fib = [1, 1]
 for i in range(number - 2):
     new_fib = fib[i] + fib[i + 1]
     fib.append(new_fib)
 
+# Display the fib values
 print(f"\nThe first {number} numbers of the Fibonacci sequence are : ")
-
 for num in fib:
     print(num)
 
+# compute the golden ratio
 golden = []
 for i in range(len(fib) - 1):
     ratio = fib[i + 1] / fib[i]
     golden.append(ratio)
 
+# Display the golden ratio and format the output
 print('\nThe corresponding Golden Ratio values are : ')
 for ratio in golden:
     print(ratio)
