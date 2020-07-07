2ddd9afc68fbbadd22b6b96401e70f29ab575ae5
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 13:35:14 +0530

    Factorial calculator app step added.

--- For Loops/FactorialCalculatorApp.py
+++ For Loops/FactorialCalculatorApp.py
@@ -6,4 +6,31 @@ the factorial. It will then use the math library to compute the value of the
 given factorial. Lastly, your program will use its own algorithm to compute
 the value of the given factorial and compare the results.
 
+Step By Step Guide:
+
+● Print a welcome message.
+● Get user input for a number to compute the factorial of.
+● Print a mathematical representation of the factorial you are about to compute.
+    ○ To accomplish this you will want to use a single print statement that prints the
+      number chosen and "! = ".
+    ○ Then you will want to use the optional argument end=" " to override the default \n
+      end of a print statement.
+    ○ Setting end=" " will end the print statement with a space and keep the prompt on
+      the same line.
+    ○ Next, use a for loop to print through the sequence of numbers needed for your
+      factorial and use an appropriate value for the end argument.
+    ○ Lastly, outside of the for loop print your last number with the default end
+      argument of to put your prompt on a new line.
+● Use the math library to compute the factorial of your given number and print the result.
+    ○ Type import math as the first line of code in your program.
+    ○ The math library has a built in function to compute a factorial you can use.
+● Write your own algorithm to compute the factorial of your given number and print the results.
+    ○ Recall that a factorial is just repeated multiplication.
+    ○ To perform this repeated multiplication, use a for loop with a numerical range.
+● Print a summary of the results to show that math library and your algorithm were in agreement.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output.
+
 """
