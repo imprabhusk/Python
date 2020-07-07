b3ccc15ddaafbf58beadab5135a759c4dfc6bdbe
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 14:36:08 +0530

    Grade point average app steps added.

--- For Loops/GradePointAverageCalculatorApp.py
+++ For Loops/GradePointAverageCalculatorApp.py
@@ -9,4 +9,30 @@ program will make a copy of the users grades and allow them to alter one of
 their previous grades to see how doing worse or better on an assignment would
 have changed their overall average.
 
+
+Step By Step Guide:
+
+● Print a welcome message.
+● Get user input for their name.
+● Get user input for the number of grades they would like to enter.
+● Create an empty list that will hold the user's grades.
+● Get user input for each individual grade and add it to the list.
+● Sort the grades from highest to lowest and print each grade individually as formatted below.
+● Calculate the average grade for the student as a float.
+● Round this average to two decimal places.
+● Display the students grade summary as formatted below.
+    ○ This summary should include the user's name, the total number of grades, the highest grade,
+      the lowest grade, and the average.
+● Prompt the user for their desired average.
+● Calculate what grade they need to get on their next assignment to achieve this average.
+● Round this grade to two decimal places.
+● Wish the user luck and inform them of the needed grade to get their desired average.
+● Make a copy of your original list of grades.
+● Get user input for a grade value that they would like to change.
+● Get user input for the new grade value they would like the old grade changed to.
+● Make the appropriate changes in the copy of the original list.
+    ○ Please keep the original grades list in tact.
+● Sort the new grades from highest to lowest and print each grade individually as formatted below.
+● Calculate the new average grade for the student as a float.
+
 """
