96c15d78d368c03353de413e7623a99a427f5941
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 14:28:36 +0530

    Fibonacci calculator app program added.

--- For Loops/FibonacciCalculatorApp.py
+++ For Loops/FibonacciCalculatorApp.py
@@ -37,3 +37,28 @@ compute.
 ● Format your output.
 
 """
+
+print("Welcome to Fibonacci Calculator App")
+
+number = int(input('\nHow many digits of the fibonacci sequence would you like to compute : '))
+
+fib = [1, 1]
+for i in range(number - 2):
+    new_fib = fib[i] + fib[i + 1]
+    fib.append(new_fib)
+
+print(f"\nThe first {number} numbers of the Fibonacci sequence are : ")
+
+for num in fib:
+    print(num)
+
+golden = []
+for i in range(len(fib) - 1):
+    ratio = fib[i + 1] / fib[i]
+    golden.append(ratio)
+
+print('\nThe corresponding Golden Ratio values are : ')
+for ratio in golden:
+    print(ratio)
+
+print('\nThe ratio of consecutive fibonacci terms approaches Phi : 1.618....')
