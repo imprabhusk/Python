bf01b8b32d25a0e45c964bdde5f282d37f7c3a06
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 13:32:52 +0530

    Factorial calculator app description added.

---
+++ For Loops/FactorialCalculatorApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will calculate the factorial of
+any given number. Your program will display the mathematical relationship of
+the factorial. It will then use the math library to compute the value of the
+given factorial. Lastly, your program will use its own algorithm to compute
+the value of the given factorial and compare the results.
+
+"""
