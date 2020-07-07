6c0af8c3c644298b38368471f3917326f9e4c686
prabhusk <prabhu.sk.169@gmail.com>
2020-07-07 12:16:17 +0530

    Binary to hexadecimal converter app description added.

---
+++ For Loops/BinaryToHexadecimalConverterApp.py
@@ -0,0 +1,11 @@
+""" Description:
+
+You are responsible for writing a program that will generate binary and
+hexadecimal values from 1 up to a specified user value. Recall that decimal is
+a base 10 number system, binary is a base 2 number system, and hexadecimal is
+a base 16 number system. Your program will use list slicing to first only show
+a portion of these values. Your program will then loop through the entire
+lists of decimal, binary, and hexadecimal values to show the relationship
+between numbers of different bases.
+
+"""
