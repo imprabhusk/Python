f4f6b903d505b9f5c9db3e8eca1ce3360a681728
prabhusk <prabhu.sk.169@gmail.com>
2020-07-08 18:42:31 +0530

    Shipping accounts app description added.

---
+++ Conditionals/ShippingAccountsApp.py
@@ -0,0 +1,11 @@
+""" Description:
+
+You are responsible for writing a program that will simulate logging into a
+business’s shipping accounts software. Once logged in your program will
+display the current costs of shipping x amount of items. Based on the number
+of items shipped, the cost to ship each item will vary. Once the cost to ship
+an item is set, your program will calculate the cost of shipping the entire
+order. Upon confirmation of the order, your program will place the order and
+prepare the shipment.
+
+"""
