78a53a570a8635c50167478794d0471f81a287ea
prabhusk <prabhu.sk.169@gmail.com>
2020-07-08 18:53:14 +0530

    Shipping accounts app steps added.

--- Conditionals/ShippingAccountsApp.py
+++ Conditionals/ShippingAccountsApp.py
@@ -8,4 +8,31 @@ an item is set, your program will calculate the cost of shipping the entire
 order. Upon confirmation of the order, your program will place the order and
 prepare the shipment.
 
+Step by Step Guide:
+
+● Begin by creating a list that will hold 5 user names of your choice.
+    ○ For example: users = ['eramom', 'footea', 'davisv', 'papinukt', 'allenj']
+● Print a welcome message to the user.
+● Get user input for their specific username.
+    ○ Usernames should not be case sensitive. For example, mike ==   Mike.
+● If the username is in the list of usernames:
+    ○ Welcome the user.
+    ○ Print the shipping price summary as formatted below.
+    ○ Get user input for how many items they would like to ship.
+    ○ Use an if, elif, else chain to determine the cost of shipping each item. 
+    ○ Calculate the users bill based on the number of items to ship and the cost
+      of shipping each item.
+        ■ Round this bill to two decimal places. 
+    ○ Print the results as formatted below.
+    ○ Ask the user if they would like to place their order. 
+    ○ If the users answer starts with y:
+        ■ Print a message confirming that their order has been placed. 
+    ○ Else:
+        ■ Inform the user that no order has been placed.
+● Else, the user does not have an account: 
+    ○ Inform the user and say goodbye.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output as below.
 """
