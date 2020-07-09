76d27db1204b2373758ca463e158496968cb637c
prabhusk <prabhu.sk.169@gmail.com>
2020-07-09 19:05:16 +0530

    Coin Toss app description added.

---
+++ Conditionals/CoinTossApp.py
@@ -0,0 +1,10 @@
+""" Description:
+
+You are responsible for writing a program that will simulate flipping a coin n
+number of times. Your program will present the user an option to see the
+result of each individual flip. Your program will also inform the user any
+time the number of heads flipped is equal to the number of tails flipped. Upon
+completion of all flips, your program will provide a summary table that shows
+the number and percentage of each flip.
+
+"""
