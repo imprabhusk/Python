0245c7da2912ac53042a8c9883a6a9e7fc318528
prabhusk <prabhu.sk.169@gmail.com>
2020-07-09 21:00:55 +0530

    Coin toss app comments added.

--- Conditionals/CoinTossApp.py
+++ Conditionals/CoinTossApp.py
@@ -51,17 +51,20 @@ import random
 print("Welcome to Coin Toss App")
 print("\nI will flip a coin a set number of times")
 
+# Get user input
 flip_number = int(input("\nHow many times would you like to flip the coin : "))
 choice = input("Would you like to see the result of each filp : ").lower()
 
 print("\nFlipping\n")
 
+# Initialize variables
 heads = 0
 tails = 0
 
 for flips in range(flip_number):
     coin = random.randint(0, 1)
 
+    # create coin object
     if coin == 1:
         heads += 1
         if choice.startswith("y"):
@@ -77,9 +80,11 @@ for flips in range(flip_number):
  each."
         )
 
+# Calculate percentages
 heads_percentage = round(100 * heads / flip_number, 2)
 tails_percentage = round(100 * tails / flip_number, 2)
 
+# print the result and format the output
 print(f"\nResults of Flipping a Coin {flip_number} Times:")
 print("\nSide\t\tCount\t\tPercentage")
 print(f"Heads\t\t{heads} / {flip_number}\t\t{heads_percentage}%")
