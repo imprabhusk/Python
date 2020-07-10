6c9dd949d44733ca8bd25e7de515d82d712ebf2e
prabhusk <prabhu.sk.169@gmail.com>
2020-07-10 13:18:22 +0530

    rock paper scissor app description added.

---
+++ Conditionals/RockPaperScissorApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will simulate playing the
+classic game Rock, Paper, Scissors against computer AI. Your program will ask
+the user how many rounds of the game they would like to play, simulate each
+round, keep score, and determine an overall winner. Your program will also
+print the classic phrases such as “Paper covers rock” or “Scissors cut paper”.
+
+"""
