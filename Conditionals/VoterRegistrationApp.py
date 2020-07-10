9be7582a2d8d898d2feaac253367d7a70d42ae55
prabhusk <prabhu.sk.169@gmail.com>
2020-07-10 12:28:17 +0530

    voter registration app description added.

---
+++ Conditionals/VoterRegistrationApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will simulate registering to
+vote. If a user is 18 or older, your program will present them with a list of
+potential political parties to register for. Upon choosing a party, your
+program will confirm that the user has registered and print a specific message
+depending on the party joined.
+
+"""
