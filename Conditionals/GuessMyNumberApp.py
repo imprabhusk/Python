a6c1c96633ca603d9d1db05f9f012394ee52ca26
prabhusk <prabhu.sk.169@gmail.com>
2020-07-10 12:55:23 +0530

    guess my number app description added.

---
+++ Conditionals/GuessMyNumberApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will play the classic “Hi Low”
+game. Your program will randomly pick a number between 1 and 20. Users will
+then guess the number. With each guess, your program will respond that the
+user’s guess is either too high or too low. When the user guesses correct, or
+after 5 guesses, your program will end the game and summarize the results.
+
+"""
