1f335b43a207c4d858a4019f5d506b0b4b6f50dc
prabhusk <prabhu.sk.169@gmail.com>
2020-07-10 12:30:56 +0530

    voter registration app steps added.

--- Conditionals/VoterRegistrationApp.py
+++ Conditionals/VoterRegistrationApp.py
@@ -6,4 +6,29 @@ potential political parties to register for. Upon choosing a party, your
 program will confirm that the user has registered and print a specific message
 depending on the party joined.
 
+Step by Step Guide:
+
+● Print a welcome message.
+● Get user input for their name.
+● Get user input for their age.
+● Create the following list that holds the following political parties:
+    ○ Republican, Democratic, Independent, Libertarian, Green]
+● If the user is old enough to vote print a message informing the user they are
+  old enough to vote.
+    ○ List all the possible political parties to join as formatted below.
+    ○ Get user input for the party they wish to join.
+    ○ If the chosen party is in the list of voting parties print a message they
+      have joined.
+        ■ If the party is republican or democratic inform the user that it is a
+          major party.
+        ■ Elif the party is independent inform the user that they are an independent
+          person.
+        ■ Else inform the user that their party is not a major party.
+    ○ Else inform the user that the party they chose is not a given party.
+● Else inform the user they are not old enough to vote.
+● Use at least 2 comments to describe sections of your code.
+● “Chunk” your code so that is readable.
+● Use appropriate and informative variable names.
+● Format your output.
+
 """
