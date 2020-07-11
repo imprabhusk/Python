c90ce3fcd26fe66791581128f51609c9305a657b
prabhusk <prabhu.sk.169@gmail.com>
2020-07-11 21:40:52 +0530

    thesaurus app description added.

---
+++ Exercise/Dictionaries/ThesaurusApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that simulates a thesaurus. Your
+program will present a user with a list of words that your thesaurus contains.
+Based on the users choice, you will randomly present them with a synonym for
+their chosen word. Lastly, your program will display all of the potential
+synonyms for each word in the thesaurus.
+
+"""
