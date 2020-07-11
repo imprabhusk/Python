9791297b595c55dd1b311d9c29fe1b592f3cfc8f
prabhusk <prabhu.sk.169@gmail.com>
2020-07-11 21:45:17 +0530

    database admin app program added.

--- Exercise/Dictionaries/DatabaseAdminApp.py
+++ Exercise/Dictionaries/DatabaseAdminApp.py
@@ -41,3 +41,72 @@ Step by Step Guide:
 ● Format your output.
 
 """
+
+print("Welcome to Database Admin App")
+
+log_on_information = {
+    "mooman74": "alskes145",
+    "meramo1986": "kehns010101",
+    "nickyD": "world1star",
+    "george2": "booo3oha",
+    "admin00": "admin1234",
+}
+
+username = input("\nEnter your username : ")
+
+# Check username exits in log_on_information or not
+if username in log_on_information.keys():
+
+    # if username exists then allow them to type their password
+    password = input("Enter your password : ")
+    if password == log_on_information[username]:
+
+        # if the user is admin then allow him to access the entire datebase
+        if username == "admin00" and password == log_on_information[username]:
+            print(f"\nHello {username}! You are logged in!")
+            print("\nHere is the current database:")
+
+            # Details of the database
+            print("-----------------------------------------------")
+            for key, value in log_on_information.items():
+                print(f"Username : {key}\t\tPassword : {value}")
+            print("-----------------------------------------------")
+
+        # if usen is not admin, then give him the options to change their passwords
+        else:
+            print(f"\nHello {username}! You are logged in.")
+            choice = input("\nWould you like to change password (yes/no): ").lower()
+
+            # Ask user whether they wish to change password are not.
+            if choice.startswith("y"):
+                new_password = input("\nSet your New Password : ")
+                if len(new_password) >= 8:
+                    confirm_password = input("Confirm your password : ")
+
+                    # if new password and confirm password same set new password to user
+                    if new_password == confirm_password:
+                        log_on_information[username] = new_password
+                        print(f"\nHello {username} your password is {new_password}")
+                        print("Your password has been set successfully")
+
+                    # if new password is not equal to typed password
+                    else:
+                        print("\nYour Typed password doesn't match")
+                        print("Please Try Again")
+
+                # if password is less than 8 characters
+                else:
+                    print("\nNote : Your password should contain minimum 8 characters")
+
+            # if user doesn't want to change the passwords, greet them for login
+            else:
+                print(f"\nThanks {username} for logging in.")
+
+    # if password is incorrect
+    else:
+        print("\nInvalid Password! Try Again")
+
+# if user doesn't exists
+else:
+    print(f"\nUsername with {username} doesn't exists on the database.")
+    print("You don't have access to log on information")
