00d67b6515361a62f0d676ad0d11a03a04518dba
prabhusk <prabhu.sk.169@gmail.com>
2020-07-12 13:29:32 +0530

    yes or no polling app description added.

---
+++ Exercise/Dictionaries/YesOrNoPollingApp.py
@@ -0,0 +1,14 @@
+""" Description:
+
+You are responsible for writing a program that will conduct a poll on a yes or
+no issue. Upon starting the program a user will be prompted for an issue to
+vote on, the number of voters, and a password to view the poll results. You
+program will then conduct the poll. Each time a user votes, your program will
+ask for the voters full name to verify that they have not yet voted. If the
+voter has not yet voted, they will be presented with the issue and can vote
+yes or no. The vote will be recorded. Once the number of voters specified by
+the user has been reached, the poll will close and a summary will be
+displayed. If the user enters the correct password a result of each voters
+name and how they voted will be displayed.
+
+"""
