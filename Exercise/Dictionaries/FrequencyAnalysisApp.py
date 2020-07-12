1f198153a93874d02beb626ff176ab62dc04a27d
prabhusk <prabhu.sk.169@gmail.com>
2020-07-12 13:33:08 +0530

    frequency analysis app description added.

---
+++ Exercise/Dictionaries/FrequencyAnalysisApp.py
@@ -0,0 +1,10 @@
+""" Description:
+
+You are responsible for writing a program that will analyse the letter
+distribution of a given text. Your program will take any text, remove all
+non-alpha characters, count the frequency of each letter within the text,
+calculate the percentage of occurrence for each letter, and create a list of
+letters ordered from highest occurrence to lowest occurrence. Your program
+will perform these operations for two different bodies of text.
+
+"""
