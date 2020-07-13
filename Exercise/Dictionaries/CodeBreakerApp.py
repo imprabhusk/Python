f0bce4c2013ba88be4dcd2217a802d6e7487a81c
prabhusk <prabhu.sk.169@gmail.com>
2020-07-13 12:17:16 +0530

    code breaker app description added.

---
+++ Exercise/Dictionaries/CodeBreakerApp.py
@@ -0,0 +1,9 @@
+""" Description:
+
+You are responsible for writing a program that will encode or decode a message
+based off the letter distribution of a predetermined key text. Your program
+will determine a frequency analysis for two texts and use these letter
+distributions to create a cipher to either encode or decode a message based
+off user input. This program is an extension of the Frequency Analysis App.
+
+"""
