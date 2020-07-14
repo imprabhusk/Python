022e8c05fe8e813bb787e860f6d4d4e449aed460
prabhusk <prabhu.sk.169@gmail.com>
2020-07-14 15:42:57 +0530

    guess my word app description added.

---
+++ Exercise/While Loops/GuessMyWordApp.py
@@ -0,0 +1,10 @@
+""" Description:
+
+You are responsible for writing a program that plays a word guessing game with
+a user. Your program will provide a category of words to the user and a string
+of dashes “-----” that represent the length of the word. The user will guess
+the word and with each incorrect guess, your program will reveal a letter at
+random, “-a---”. Upon guessing the word correctly, your program will then
+inform the user how many guesses they took.
+
+"""
