c3d13aed285bc06aa06eafc327e4b88686329163
prabhusk <prabhu.sk.169@gmail.com>
2020-07-14 13:53:22 +0530

    even or odd number sorter app description added.

---
+++ Exercise/While Loops/EvenOddNumberSorterApp.py
@@ -0,0 +1,7 @@
+""" Description:
+
+You are responsible for writing a program that sorts a list of comma separated
+numbers as either even or odd. Upon sorting the numbers into two groups, your
+program will then sort each group numerically and display the results.
+
+"""
