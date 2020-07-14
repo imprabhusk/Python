9004d442dc8994c10e59ab00720ddc043c6f45fa
prabhusk <prabhu.sk.169@gmail.com>
2020-07-14 14:51:31 +0530

    prime number app description added.

---
+++ Exercise/While Loops/PrimeNumberApp.py
@@ -0,0 +1,10 @@
+""" Description:
+
+You are responsible for writing a program that will either determine if a
+given number is prime or display all prime numbers within a given range of
+values. When determining all prime numbers within a given range, your program
+will time the process and report how long the calculations took to the user.
+It is important to time certain processes within our programs so we can so how
+efficient our code is.
+
+"""
