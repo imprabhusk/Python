0c5f5405c23586fda08482040e521a4c3b7c03f8
prabhusk <prabhu.sk.169@gmail.com>
2020-07-14 13:11:00 +0530

    factor generator app description added.

---
+++ Exercise/While Loops/FactorGeneratorApp.py
@@ -0,0 +1,8 @@
+""" Description:
+
+You are responsible for writing a program that generates all factors of a
+given number. Your program will display the factors individually and give a
+mathematically summary of how different pairs of factors can be multiplied
+together to get the given number.
+
+"""
