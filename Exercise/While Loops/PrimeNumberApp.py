e99451043568247db6e806068bc44e5a86e5df84
prabhusk <prabhu.sk.169@gmail.com>
2020-07-14 15:34:05 +0530

    prime number app program added.

--- Exercise/While Loops/PrimeNumberApp.py
+++ Exercise/While Loops/PrimeNumberApp.py
@@ -86,3 +86,69 @@ Step by Step Guide:
 ● Format your output.
 
 """
+
+import time
+
+print("Welcome to Prime Number App")
+
+running = True
+while running:
+
+    print("\nEnter 1 to determine if a specific number is prime. ")
+    print("Enter 2 to determine all prime number within a set range. ")
+
+    option = int(input("\nEnter your choice 1 or 2 : "))
+
+    if option == 1:
+        number = int(input("\nEnter a number to determine if it is prime or not : "))
+
+        prime_status = True
+        for i in range(2, number // 2):
+            if number % i == 0:
+                prime_status = False
+                break
+
+        if prime_status:
+            print(f"{number} is a Prime Number")
+        else:
+            print(f"{number} is Not a Prime Number")
+
+    elif option == 2:
+        lower_bound = int(input("\nEnter the lower bound of your range : "))
+        upper_bound = int(input("Enter the upper bound of your range : "))
+
+        primes = []
+
+        start_time = time.time()
+
+        for prime_candidate in range(lower_bound, upper_bound + 1):
+            if prime_candidate > 1:
+                prime_status = True
+                for i in range(2, prime_candidate):
+                    if prime_candidate % i == 0:
+                        prime_status = False
+                        break
+            else:
+                prime_status = False
+
+            if prime_status:
+                primes.append(prime_candidate)
+
+        end_time = time.time()
+
+        delta_time = round(end_time - start_time, 4)
+        print(f"\nCongratulations, It took a total of {delta_time} seconds.")
+        print(
+            f"The following numbers between {lower_bound} and {upper_bound} are prime : "
+        )
+
+        for prime in primes:
+            print(prime)
+
+    else:
+        print("\nThat's not a valid option.")
+
+    choice = input("\nWould you like to run the program again (y/n) : ").lower()
+    if choice != "y":
+        running = False
+        print("\nThank you for using the prime number app. Have a great day")
