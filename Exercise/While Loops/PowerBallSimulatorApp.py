8e26bfc9b4847738af6247d9634b7834ddce4b24
prabhusk <prabhu.sk.169@gmail.com>
2020-08-15 12:52:25 +0530

    power ball simulator app description added.

---
+++ Exercise/While Loops/PowerBallSimulatorApp.py
@@ -0,0 +1,13 @@
+""" Description
+
+You are responsible for writing a program that simulates the Power Ball
+Lottery. The traditional power ball is played by randomly choosing 5 white
+balls numbered 1 through 69 then randomly choosing 1 red ball numbered 1
+through 26. The traditional power ball has astronomically low odds of
+winning. Therefore, your program will allow users to adjust the odds by
+setting how many balls the lottery will choose from. Your program will
+then calculate the odds the user has of winning. The user will purchase
+tickets in a set interval, without purchasing repeated losing tickets,
+until they either win the lottery or choose to give up.
+
+"""
