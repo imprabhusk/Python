7920d509aa2c1c157c1a3d87abb70248d9024066
prabhusk <prabhu.sk.169@gmail.com>
2020-08-23 19:49:31 +0530

    dice app program descrpition added.

---
+++ Exercise/Functions/TheDiceApp.py
@@ -0,0 +1,7 @@
+""" Description :
+
+You are responsible for writing a program that will roll a given number of
+dice of any number of sides and sum the total. Your program will continue to
+run until the user desires to quit.
+
+"""
